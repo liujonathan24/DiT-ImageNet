310853e437f0a0c79977bf9374e0876f00719b32
Jonathan Liu <liujonathan24@gmail.com>
2025-10-12 10:52:10 -0400

    Working weight inspection function

--- inspect_weights.py
+++ inspect_weights.py
@@ -1,8 +1,12 @@
 from diffusion_transformer import DiffusionTransformer
-from helpers.config import modelConfig
+from helpers.config import modelConfig, trainConfig
 import jax
 import jax.numpy as jnp
 from flax.nnx.statelib import State
+from flax import nnx
+import argparse
+import os
+from helpers.checkpoint import save_checkpoint, restore_checkpoint
 
 def print_param_stats(params, path=''):
     for key, value in params.items():
@@ -14,18 +18,35 @@ def print_param_stats(params, path=''):
             print(f"  mean: {jnp.mean(value)}")
             print(f"  std: {jnp.std(value)}")
 
-def main():
-    # Load model configuration
-    config = modelConfig()
+def main(args):
+    if jax.devices("gpu"):
+        gpu_device = jax.devices("gpu")[0]
+    else:
+        gpu_device = None
+    assert gpu_device != None
 
-    # Create the model
-    model = DiffusionTransformer(config)
+    # Load model configuration
+    modelconfig = modelConfig()
+    trainconfig = trainConfig()
+    
+    # Load model if desired:
+    if args.resume:
+        experiment_path = args.resume
+        models_dir = os.path.abspath(os.path.join(experiment_path, "models"))
+
+        model, extra_params = restore_checkpoint(models_dir, modelconfig, trainconfig, gpu_device)
+    else:
+        # Create the model
+        model = DiffusionTransformer(modelconfig)
 
     # Get the model's parameters
-    params = model.get_state()
+    params = nnx.state(model)
 
     # Print the statistics for each parameter
     print_param_stats(params)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--resume", "-re", help="Path to the experiment directory to resume training from.")
+    args = parser.parse_args()
+    main(args)
