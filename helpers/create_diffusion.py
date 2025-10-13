426958d98b041eb202583938899ad8ac2049c37f
Jonathan Liu <liujonathan24@gmail.com>
2025-10-13 01:21:35 -0400

    update

---
+++ helpers/create_diffusion.py
@@ -0,0 +1,33 @@
+import jax.numpy as jnp
+import numpy as np
+
+from .diffusion import GaussianDiffusion
+
+# This file is a JAX/NumPy port of the original PyTorch implementation in
+# https://github.com/openai/DiT/blob/main/diffusion/__init__.py
+
+def get_named_beta_schedule(schedule_name, num_diffusion_timesteps):
+    if schedule_name == "linear":
+        scale = 1000 / num_diffusion_timesteps
+        beta_start = scale * 0.0001
+        beta_end = scale * 0.02
+        return np.linspace(beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64)
+    else:
+        raise NotImplementedError(f"unknown beta schedule: {schedule_name}")
+
+
+def create_diffusion(steps, noise_schedule="linear", model_mean_type="epsilon", model_var_type="learned_range"):
+    betas = get_named_beta_schedule(noise_schedule, steps)
+    
+    # For now, we hardcode the mean and var types from the DiT paper
+    # In a real training script, these would be configurable
+    if model_mean_type != "epsilon":
+        raise ValueError("Only epsilon prediction is supported.")
+    if model_var_type != "learned_range":
+        raise ValueError("Only LEARNED_RANGE is supported.")
+
+    return GaussianDiffusion(
+        betas=betas,
+        model_mean_type=model_mean_type, # "epsilon"
+        model_var_type=model_var_type, # "learned_range"
+    )
